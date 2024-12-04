8ad2461ef22a3dc5eec3fb5a51967820b56ba421
dek226 <123472705+dek226@users.noreply.github.com>
2024-12-03 23:35:38 -0500

    Update anode_gen.v

--- anode_gen.v
+++ anode_gen.v
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Demetri Kostas
+// Create Date: 11/22/2024 02:15:19 PM
+// Module Name: anode_gen
+// Project Name: Multiplier
+// Revision:
+// Revision 0.01 - File Created
+//////////////////////////////////////////////////////////////////////////////////
 module anode_gen(clk, seg_anode);
 	input wire clk;
 	output wire [3:0] seg_anode;
